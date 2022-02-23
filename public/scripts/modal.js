884a890acf0c7d1747429f500cbce639fc8558cf
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-23 16:47:43 -0300

    Update modal.js

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,1 +1,16 @@
-//
+//código especifico da modal
+module.exports = function Modal() {
+    function open() {
+        
+    }
+
+    function close() {
+        
+    }
+
+    //retornando as funcionalidades acima
+    return {
+        open,
+        close
+    }
+}
