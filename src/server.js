cf4ef122236d87836966600bca1126b83b2e4cb9
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-23 11:21:50 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 //responsável por iniciar nosso servidor.
 
 //importando o express
-const express = require('express');
+const express = require('express')
 
 //importanto o route
-const route = require('./route');
+const route = require('./route')
 
 //criando o server
 const server = express()
+server.use(route)
 
 //pegando o server e passando uma porta p/ ele
 server.listen(3000, () => console.log("rodando!"))
