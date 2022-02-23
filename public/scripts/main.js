002158ec0111ea9650e1eac940930e3eebde62a2
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-23 16:54:34 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,4 +3,12 @@
 //importando o arquivo da modal
 import { Modal } from './modal'
 
+const modal = Modal()
 
+//pegar botões com class="check"
+
+//pegar quando o 'marcar como lido' for clicado.
+
+
+
+modal.open() //abrir modal
