d305b06a2899cb401c8b74cfdf92b9e74b605650
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-23 11:12:15 -0300

    Update route.js

--- src/route.js
+++ src/route.js
@@ -7,4 +7,5 @@ const route = express.Router()
 //definindo nossas rotas.
 //req = requisição, res = resposta
 //o index aqui é = index.ejs
+// o / é o meu domínio
 route.get('/', (req, res) => res.render("index"))
