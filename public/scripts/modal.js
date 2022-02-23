a0a5270297a974eddec075b0bbe3c7fc13ee9539
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-23 16:54:59 -0300

    Update modal.js

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,12 @@
 //código especifico da modal
 module.exports = function Modal() {
     function open() {
-        
+        //funcionalidade de atribuir a classe active para a modal.
+
     }
 
     function close() {
+        //funcionalidade de remover a classe active e fechar a modal.
         
     }
 
