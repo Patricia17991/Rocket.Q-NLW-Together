0330fc7d50da23e907901de84fdde0558325edc4
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-24 12:33:23 -0300

    Update modal.js

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -3,6 +3,8 @@ export default function Modal() {
     
     const cancelButton = document.querySelector('.button.cancel')
     
+    cancelButton.addEventListener("click", close)
+    
     function open() {
         //funcionalidade de atribuir a classe active para a modal.
          document.querySelector('.modal-wrapper').classList.add("active")
