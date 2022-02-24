e7ede1872e5cab8819471712590a5efe60cc4719
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-24 11:30:46 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,12 +8,11 @@ const checkButtons = document.querySelectorAll("a.actions a.check")//pegar botõ
 
 //usamos o eventlistener p/ saber quando algum botão foi clicado por exemplo.
 checkButtons.forEach( button => {
-    button.eventlistener("click")
-    
+    button.eventlistener("click", {
+        //aqui: o que vai fazer quando escutar o click.
+        modal.open() //abrir modal
+    })
+
 }) //percorrer todos os botões.
 
 //pegar quando o 'marcar como lido' for clicado.
-
-
-
-modal.open() //abrir modal
