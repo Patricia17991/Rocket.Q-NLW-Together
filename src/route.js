9bbbfd9166f2379366c790c24122c91f39f611fc
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-25 17:05:00 -0300

    Update route.js

--- src/route.js
+++ src/route.js
@@ -9,5 +9,10 @@ const route = express.Router()
 //o index aqui é = index.ejs
 // o / é o meu domínio
 route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
+route.get('/room', (req, res) => res.render("room")
+)
 route.get('/create-pass',(req, res) => res.render("create-pass"))
+route.get('/room/:room/:question/:action', (req, res) => res.render()) //usamos os ':' pq aqui não sabemos os ids, os conteúdos que virão.
+
+module.exports = route
+//o browser aparentmente so consegue fazer a função get
