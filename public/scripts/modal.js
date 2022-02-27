f248a64931b34086a0de27eb8f0fa4de4e9f30b4
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-26 21:16:36 -0300

    Update modal.js

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,7 +1,4 @@
-//código especifico da modal
-
-
-export default function Modal() {
+    export default function Modal() {
 
     const modalWrapper = document.querySelector('.modal-wrapper') //para não precisar repetir o código tirado
 
@@ -10,13 +7,11 @@ export default function Modal() {
     cancelButton.addEventListener("click", close)
 
     function open() {
-        //funcionalidade de atribuir a classe active para a modal.
         modalWrapper.classList.add("active")
 
     }
 
     function close() {
-        //funcionalidade de remover a classe active e fechar a modal.
         modalWrapper.classList.remove("active")
         
     }
