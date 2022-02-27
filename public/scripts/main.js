e4ccda3cdf90c4cc46cd9099882fff64cf9ea13d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-26 21:17:37 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,10 @@
 //aqui colocamos todo o código de script que tivermos
-
 import Modal from './modal.js'//importando o arquivo da modal
 
 const modal = Modal()
-
 const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
-
 const checkButtons = document.querySelectorAll("a.actions a.check")//pegar botões com class="check"
 
 //usamos o eventlistener p/ saber quando algum botão foi clicado por exemplo.
@@ -15,9 +12,8 @@ checkButtons.forEach( button => {
     button.addEventlistener("click", handleClick)
       
 
-}) //percorrer todos os botões.(código p/marcar como lido)
+}) //percorrer todos os botões
 
-//botão excluir
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
@@ -43,6 +39,6 @@ function handleClick(event, check = true) {
     
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red") //remove ou coloca a class button, p/ só o botão excluir ficar vermelho 
 
-    modal.open()//abrir modal
+    modal.open()
 }
 
