b9ba4b07a684e3b49b183b02e81cb24b74618780
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-26 21:14:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,17 +3,11 @@ const express = require('express')
 
 const route = require('./route')//importanto o route
 const path = require('path')//modulo que usaremos p/ colocar rota da pasta views
-
-
 const server = express()//criando o server
 
 server.set('view engine', 'ejs')//configurando o ejs
-
 server.use(express.static("public"))//dizendo p/ express que vamos usar a pasta public
-
-
 server.set('views',path.join(__dirname, 'views'))//dizendo onde esta a pasta View
-
 server.use(route)
 
 
