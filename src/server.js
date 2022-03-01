6fc6409c8b79fc462a1636284fe63610d2ece1f1
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-01 16:49:14 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
-//importando o express
 const express = require('express')
 const route = require('./route')
-
 const path = require('path')//modulo que usaremos p/ colocar rota da pasta views
-const server = express()//criando o server
 
-server.set('view engine', 'ejs')//configurando o ejs
-server.use(express.static("public"))//dizendo p/ express que vamos usar a pasta public
-server.set('views',path.join(__dirname, 'views'))//dizendo onde esta a pasta View
+//criando o server
+const server = express()
+
+//configurando o ejs
+server.set('view engine', 'ejs')
+
+server.use(express.static("public"))
+
+//dizendo onde esta a pasta View
+server.set('views',path.join(__dirname, 'views'))
+
+//sempre antes d rota, configurando midure
+server.use(express.urlencoded({extended: true})) //o form vem aqui antes de ir pra controller
+
 server.use(route)
 
-
 //pegando o server e passando uma porta p/ ele
-server.listen(3000, () => console.log("rodando!"))  
+server.listen(3000, () => console.log("rodando!"))
+
 
 
 
