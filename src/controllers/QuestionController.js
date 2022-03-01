cb20458df03cc661cee943ec6cfd3762399f4110
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-01 16:28:09 -0300

    Update QuestionController.js

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,6 +1,10 @@
 //vamos mandar o conteúdo da modal para cá
 module.exports = {
-    index() {
+    index(req, res){
+        //separar variáveis
+        const roomId = req.params.room
+        const questionId = req.params.room
+        const action = req.params.action
         
     }
 }
