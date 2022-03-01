58144bce163d860f389379eef626893bd19b192d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-01 16:24:33 -0300

    Update route.js

--- src/route.js
+++ src/route.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 //falando quem é o route
 const route = express.Router()
-route.get('/', (req, res) => res.render("index"))// o / é o meu domínio
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass',(req, res) => res.render("create-pass"))
 
-//exemplo do formato da rota:
-route.post('/room/:room/:question/:action')
+const QuestionController = require('./controllers/QuestionController')
+
+//o index aqui é = index.ejs
+// o / é o meu domínio
+route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req, res) => res.render("room")
+)
+route.get('/create-pass', (req, res) => res.render("create-pass"))
+
+//exemplo do formato que vamos passar p/ rota
+route.post('/room/:room/:question/:action', QuestionController.index)
 //usamos os ':' pq aqui não sabemos os ids, os conteúdos que virão.
 
 module.exports = route
-
+//o browser  so consegue fazer a função get
 
