6bca7c80be2fc45271ddc641dd7b12837ed2f25e
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-02 16:01:21 -0300

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,3 +1,11 @@
 //configurações do banco de dados
 const sqlite3 = require("sqlite3") 
 const {open} = require("sqlite") 
+
+module.exports = () => {
+    open({
+        filename:'/src/db/rocketq.lite' //caminho do nosso banco de dados
+    })
+}
+
+//rocketq.lite é o nome que escolhemos para o nosso banco de dados
