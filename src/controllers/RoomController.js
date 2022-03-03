ff342a140d8797cbe3fa261e239aa91142a98fbe
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-03 18:37:03 -0300

    Update RoomController.js

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -11,7 +11,7 @@ module.exports = {
            roomId += Math.floor(Math.random() * 10).toString() //transformou os números de 0 a 9 em string e concatenou eles para formar uma sequencia do tipo 365847
         }
         //inserir no banco de dados
-        db.run(``)//aqui dentro os comandos insert, para fazermos a inserção no banco de dados
+        await db.run(``)//aqui dentro os comandos insert, para fazermos a inserção no banco de dados
 
         db.close()
         
