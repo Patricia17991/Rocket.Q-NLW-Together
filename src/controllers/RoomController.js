a3d63d5d787457e61c1f89d2bd8fdd5956b02a47
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-03 18:28:28 -0300

    Update RoomController.js

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,9 +1,10 @@
 module.exports = {
     create(req, res) {
         //código p/ criar salas novas
-        let roomId = null
+        let roomId 
 
         for (var i = 0; i = 6; i++){
+            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
            roomId += Math.floor(Math.random() * 10).toString() //transformou os números de 0 a 9 em string e concatenou eles para formar uma sequencia do tipo 365847
         }
         
