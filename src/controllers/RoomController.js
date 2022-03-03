721c7be7ce3ec798f2e4d6cad96c5bbbf70e5723
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-03 18:17:48 -0300

    Update RoomController.js

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,7 +4,7 @@ module.exports = {
         let roomId = 0
 
         for (var i = 0; i = 6; i++){
-            Math.floor(Math.random() * 10) //vai gerar nossos números aleatórios de um a 9
+           roomId += Math.floor(Math.random() * 10) //vai gerar nossos números aleatórios de um a 9
         }
         
        
