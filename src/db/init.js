d601dc4cedac8be49605ddbb91c2bd07611ad6d3
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-03 17:26:19 -0300

    Update init.js

--- src/db/init.js
+++ src/db/init.js
@@ -10,16 +10,18 @@ const initDb = {
         const db = await Database()
 
         //primeiro comando para criar as tabelas
-        await db.exec(`
-          
-        `)//onde colocamos o código p/ criar as tabelas (onde ficam os códigos SQL).Comandos sql sempre em maiúsculo e o que não for comando em minúsculo
+        await db.exec(`CREATE TABLE rooms(
+           id INTERGER PRIMARY KEY,
+           pass TEXT
+        )`) //onde coloco o código para criar a nossa tabela (onde ficam os códigos SQL).Comandos  sql sempre em maiúculo e o que não for comando em minúculo
     }
 
 } //iniciando nosso banco de dados
 
-
 initDb.init() //chamar/executar a função
 
 //esse arquivo roda separadamente fora do projeto
-//o AWAIT é uma forma de garantir que o banco de dados vai ser executado da forma correta antes de fazer o que precisa ser feito(para evitar erros na execução)
+
+//o AWAIT é uma forma de garantir que o banco de dados vai ser executado da forma correta antes de fazer o que precisa ser feito (para evitar erros na excução)
+
 //o JS não permiti aqui rodar sem o ASYNC (o await não funciona sem o async)
