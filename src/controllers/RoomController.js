a08deb3f02cf2c0a704f382a0cf9e6439c7fa580
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-03 18:17:12 -0300

    Update RoomController.js

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,12 @@
 module.exports = {
     create(req, res) {
         //código p/ criar salas novas
-        let roomId = 123456 //vamos criar esse número dinâmicamente com a função randle
-        Math.floor(Math.random() * 10) //vai gerar nossos números aleatórios de um a 9
+        let roomId = 0
+
+        for (var i = 0; i = 6; i++){
+            Math.floor(Math.random() * 10) //vai gerar nossos números aleatórios de um a 9
+        }
+        
        
         res.redirect(`/room/${roomId}`)
     }
