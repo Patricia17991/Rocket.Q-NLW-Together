7f1923b9af50ccd2f57baf4315f2c7ded5ad6cad
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-04 19:02:39 -0300

    Update route.js

--- src/route.js
+++ src/route.js
@@ -4,16 +4,14 @@ const RoomController = require('./controllers/RoomController')
 
 const express = require('express');
 
-//o index aqui é = index.ejs
+//o index aqui é o index.ejs
 // o / é o meu domínio
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
-route.get('/create-pass', (req, res) => res.render("index", {page: create-pass}))//passando uma variável aqui dentro
-
+route.get('/create-pass', (req, res) => res.render("index", {page: create-pass}))
 route.get('/room/:room', (req, res) => res.render("room"))
 
 //exemplo do formato que vamos passar p/ rota
 route.post('/question/:room/:question/:action', QuestionController.index)
-//usamos os ':' pq aqui não sabemos os ids, os conteúdos que virão.
 route.post('/create-room', RoomController.create)
 
 module.exports = route
