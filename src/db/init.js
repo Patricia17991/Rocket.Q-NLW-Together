0fac0182afed266adfa8550bbe7729b05ecc6492
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-07 11:54:22 -0300

    Update init.js

--- src/db/init.js
+++ src/db/init.js
@@ -1,34 +1,24 @@
-//se for usar o await dentro de uma função ela precisará ser uma função async
-
-//primeiro importar nosso banco de dados
 const Database = require("./config")
 
 const initDb = {
-    //todas as nossas funções
-    async init() {
-        //criando o banco de dados em si.
+    async init(){
         const db = await Database()
 
-        //primeiro comando para criar as tabelas
-        await db.exec(`CREATE TABLE rooms(
-           id INTEGER PRIMARY KEY,
-           pass TEXT
-        )`); //onde coloco o código para criar a nossa tabela (onde ficam os códigos SQL).Comandos  sql sempre em maiúculo e o que não for comando em minúculo.
-        await db.exec(`CREATE TABLE questions(
-           id INTEGER PRIMARY KEY AUTOINCREMENT,
-           titulo TEXT,
-           read INT
-
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
         )`);
+
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`);
+
         await db.close()
     }
+}
 
-} //iniciando nosso banco de dados. quando iniciamos um database é interessante fechar ele depois da execução
+initDb.init();
 
-initDb.init() //chamar/executar a função
-
-//esse arquivo roda separadamente fora do projeto
-
-//o AWAIT é uma forma de garantir que o banco de dados vai ser executado da forma correta antes de fazer o que precisa ser feito (para evitar erros na excução)
-
-//o JS não permiti aqui rodar sem o ASYNC (o await não funciona sem o async)
