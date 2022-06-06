bbf0ae4c63e4143b292a27c2e6857b97c90da718
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-06 17:55:03 -0300

    Update QuestionController.js

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,11 +1,11 @@
 //vamos mandar o conteúdo da modal para cá
 module.exports = {
     index(req, res){
-        //separar variáveis
+
         const roomId = req.params.room
         const questionId = req.params.room
         const action = req.params.action
-        const password = req.body.password //name do input do form q/ aqui é password
+        const password = req.body.password 
 
 
         console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
